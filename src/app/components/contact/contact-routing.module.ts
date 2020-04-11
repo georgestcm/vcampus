1d320c139f15828e68c1c5c9a44f96ccf7bc64f8
georges tchianga <georgestcm@gmail.com>
2020-04-11 05:09:25 -0400

    Remove path and component for contact page

--- src/app/components/contact/contact-routing.module.ts
+++ src/app/components/contact/contact-routing.module.ts
@@ -4,10 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContactPage } from './contact.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ContactPage
-  }
 ];
 
 @NgModule({
