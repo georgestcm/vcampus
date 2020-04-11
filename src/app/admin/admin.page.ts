f2366c41ef4cb9cde06891bbe9ddd59e69d5730e
georges tchianga <georgestcm@gmail.com>
2020-04-11 05:05:28 -0400

    Add methods for admin navigation

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.page.html',
@@ -7,12 +8,49 @@ import { Router } from '@angular/router';
 })
 export class AdminPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private storage: Storage) { }
+     userName = {
+       first_name:'',
+       last_name:''
+     }
 
   ngOnInit() {
+    this.storage.get('user').then((val) => {
+    this.userName.first_name = val.first_name;
+    this.userName.last_name = val.last_name;
+  })
   }
 
+  userData;
   toDashboard(){
      this.router.navigate(['admin/dashboard'])
    }
+
+   toPermissions(){
+      this.router.navigate(['admin/permissions'])
+    }
+
+    toSchoolList(){
+      this.router.navigate(['admin/school-list'])
+    }
+
+    toStudentList(){
+      this.router.navigate(['admin/student-list'])
+    }
+
+    toTeacherList(){
+      this.router.navigate(['admin/teacher-list'])
+    }
+
+    toCourseList(){
+      this.router.navigate(['admin/courses-list'])
+    }
+
+    logoutUser(){
+      this.storage.remove('token')
+      this.storage.remove('user')
+      this.storage.remove('role')
+     this.router.navigate(['/login'])
+
+    }
 }
