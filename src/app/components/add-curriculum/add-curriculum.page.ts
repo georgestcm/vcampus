a7939d0078b10306fdb4e127541935d5546b5bdc
georges tchianga <georgestcm@gmail.com>
2020-04-16 23:58:34 -0400

    Add methods to get and post curriculum

--- src/app/components/add-curriculum/add-curriculum.page.ts
+++ src/app/components/add-curriculum/add-curriculum.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import {AuthService } from 'src/app/auth.service';
+import {CourseService } from 'src/app/course.service';
 @Component({
   selector: 'app-add-curriculum',
   templateUrl: './add-curriculum.page.html',
@@ -9,14 +9,49 @@ import {AuthService } from 'src/app/auth.service';
 export class AddCurriculumPage implements OnInit {
 
   constructor(private storage: Storage,
-  private _auth: AuthService) { }
+  private _course: CourseService) { }
  error;
  myCurrentSchoolId;
+ curList = []
+ addCur = {
+   _id:'',
+   curriculum:''
+ }
   ngOnInit(){
     this.storage.get('user').then((val) => {
     this.myCurrentSchoolId = val._id;
-  console.log(this.myCurrentSchoolId)
+    this.getCurr(val._id)
   })
   }
 
+ getCurr(id){
+   this._course.getCurriculumList(id)
+   .subscribe(
+     res=>(
+       this.curList.push(res)
+     ),
+     err =>(
+       console.log(err)
+     )
+   )
+ }
+
+ addCurr(){
+   this.addCur._id = this.myCurrentSchoolId
+   if(this.addCur.curriculum.length===0){
+     this.error = "Please write a curriculum"
+   } else {
+     this.addCur.curriculum.trim().charAt(0).toUpperCase()
+     this._course.postCur(this.addCur)
+        .subscribe(
+          res=>(
+            this.getCurriculumList(this.myCurrentSchoolId),
+            console.log(res)
+          ),
+          err=>(
+            this.error=err.error
+          )
+        )
+   }
+ }
 }
